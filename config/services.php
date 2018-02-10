013ef2c13f6404f3c15fa807ff15196085789ed3
WIN-JCSC4V55IQ1\User <avadakkeddavra@gmail.com>
2018-02-10 12:38:02 +0200

    init a project

---
+++ config/services.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+    ],
+
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => 'us-east-1',
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe' => [
+        'model' => App\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+    'google' => [
+        'client_id' => '723160927970-cil33tpr3hfmttju0iukmskb97288tur.apps.googleusercontent.com',
+        'client_secret' => '1QEgZ7MZVlb9-b-UmBTY6y_b',
+        'redirect' => 'http://light-it.loc/auth/google',
+    ],
+];
